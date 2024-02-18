24030c94cb359f6a1325f2141721318ee1b4ed0b
Björn Larsson <robjorn_larsson@hotmail.com>
2024-02-18 10:54:11 +0000

    Update settings file with allowed host, verify site opens in local host

--- tothe_list/settings.py
+++ tothe_list/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7x+nn$ollq9hjp6e)5-o6r$t@n8!azl3&5a_uzhg_z=roq_hme
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-bjornl1-tothelist-votza1jtgrm.ws-eu108.gitpod.io']
 
 
 # Application definition
